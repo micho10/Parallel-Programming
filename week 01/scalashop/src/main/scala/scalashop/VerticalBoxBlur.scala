4967362e9b230de3b23648c628375682bad461b6
micho <gatin13@yahoo.com>
2016-09-04 23:56:21 +0900

    Completed the vertical box blur

--- week 01/scalashop/src/main/scala/scalashop/VerticalBoxBlur.scala
+++ week 01/scalashop/src/main/scala/scalashop/VerticalBoxBlur.scala
@@ -44,7 +44,11 @@ object VerticalBoxBlur {
    */
   def blur(src: Img, dst: Img, from: Int, end: Int, radius: Int): Unit = {
     // TODO implement this method using the `boxBlurKernel` method
-    ???
+    for (
+      x <- from until end;
+      y <- 0 until src.height
+    ) yield dst(x, y) = boxBlurKernel(src, x, y, radius)
+
   }
 
   /** Blurs the columns of the source image in parallel using `numTasks` tasks.
@@ -55,7 +59,11 @@ object VerticalBoxBlur {
    */
   def parBlur(src: Img, dst: Img, numTasks: Int, radius: Int): Unit = {
     // TODO implement using the `task` construct and the `blur` method
-    ???
+    val strip = 0 to src.width by ( src.width / math.min( numTasks, src.width ) )
+    val ranges = strip zip strip.tail
+    val tasks = ranges map {
+      case (from, to) => task( blur( src, dst, from, to, radius ) )
+    }
   }
 
 }
